b003289cdda27804865c0107701ee02824069fd6
Mohamed Nagy <mohamednagy2015@outlook.com>
2018-03-21 16:20:46 -0700

    retrive data and test completed

--- app/src/main/java/com/adja/apps/mohamednagy/bakingapp/MainActivity.java
+++ app/src/main/java/com/adja/apps/mohamednagy/bakingapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.adja.apps.mohamednagy.bakingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.adja.apps.mohamednagy.bakingapp.model.Recipe;
 import com.adja.apps.mohamednagy.bakingapp.network.NetworkHandler;
@@ -12,7 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NetworkHandler networkHandler = new NetworkHandler(getBaseContext()) {
+            @Override
+            protected void onPostExecute(Recipe[] recipes) {
+                Log.e("data done", String.valueOf(recipes.length));
+            }
 
+            @Override
+            protected void onFailure(String message) {
+                Log.e("error", message);
+            }
+        };
+        networkHandler.execute();
     }
 
 }
