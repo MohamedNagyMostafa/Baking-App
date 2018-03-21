09ea4f229fbdfde38944b1bedee13f779341495c
Mohamed Nagy <mohamednagy2015@outlook.com>
2018-03-21 16:16:28 -0700

    network validation implement

--- app/src/main/java/com/adja/apps/mohamednagy/bakingapp/network/NetworkHandler.java
+++ app/src/main/java/com/adja/apps/mohamednagy/bakingapp/network/NetworkHandler.java
@@ -1,6 +1,12 @@
 package com.adja.apps.mohamednagy.bakingapp.network;
 
+import android.content.Context;
+import android.net.NetworkInfo;
+
+import com.adja.apps.mohamednagy.bakingapp.R;
 import com.adja.apps.mohamednagy.bakingapp.model.Recipe;
+import com.adja.apps.mohamednagy.bakingapp.network.helper.NetworkState;
+import com.adja.apps.mohamednagy.bakingapp.permission.PermissionHandler;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -12,8 +18,10 @@ import retrofit2.Response;
 
 public abstract class NetworkHandler {
     private RecipeClient mRecipeClient;
+    private Context mContext;
 
-    public NetworkHandler(){
+    public NetworkHandler(Context context){
+        mContext = context;
         init();
     }
 
@@ -23,19 +31,37 @@ public abstract class NetworkHandler {
     }
 
     public void execute(){
-        Call<Recipe[]> call = mRecipeClient.recipesResponse();
-        call.enqueue(new Callback<Recipe[]>() {
-            @Override
-            public void onResponse(Call<Recipe[]> call, Response<Recipe[]> response) {
-                onPostExecute(response.body());
-            }
+        if(testNetwork()) {
+            Call<Recipe[]> call = mRecipeClient.recipesResponse();
+            call.enqueue(new Callback<Recipe[]>() {
+                @Override
+                public void onResponse(Call<Recipe[]> call, Response<Recipe[]> response) {
+                    onPostExecute(response.body());
+                }
 
-            @Override
-            public void onFailure(Call<Recipe[]> call, Throwable t) {
-                //TODO: show msg to user.
+                @Override
+                public void onFailure(Call<Recipe[]> call, Throwable t) {
+                    //TODO: show msg to user.
+                    NetworkHandler.this.onFailure(t.getMessage());
+                }
+            });
+        }
+    }
+
+    private boolean testNetwork(){
+        if(PermissionHandler.checkPermission(PermissionHandler.INTERNET_PERMISSION, mContext) &&
+                PermissionHandler.checkPermission(PermissionHandler.ACCESS_INTERNET_STATE, mContext)){
+            if(NetworkState.hasInternet(mContext)){
+                return true;
+            }else{
+                onFailure(mContext.getResources().getString(R.string.no_network_connection));
             }
-        });
+        }else{
+            PermissionHandler.askPermission(mContext, PermissionHandler.ACCESS_INTERNET_STATE, PermissionHandler.INTERNET_PERMISSION);
+        }
+        return false;
     }
 
     protected abstract void onPostExecute(Recipe[] recipes);
+    protected abstract void onFailure(String message);
 }
