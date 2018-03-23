485614ab0e2008ae511987a3625b3b8ae0b3274e
Mohamed Nagy <mohamednagy2015@outlook.com>
2018-03-23 07:37:19 -0700

    media player implement-testing

--- app/src/main/java/com/adja/apps/mohamednagy/bakingapp/MainActivity.java
+++ app/src/main/java/com/adja/apps/mohamednagy/bakingapp/MainActivity.java
@@ -2,35 +2,39 @@ package com.adja.apps.mohamednagy.bakingapp;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.adja.apps.mohamednagy.bakingapp.databinding.ActivityMainBinding;
+import com.adja.apps.mohamednagy.bakingapp.media.Media;
 import com.adja.apps.mohamednagy.bakingapp.model.Recipe;
 import com.adja.apps.mohamednagy.bakingapp.network.NetworkHandler;
 import com.adja.apps.mohamednagy.bakingapp.permission.PermissionHandler;
 
 public class MainActivity extends AppCompatActivity {
 
-    private NetworkHandler networkHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        networkHandler = new NetworkHandler(this) {
-            @Override
-            protected void onPostExecute(Recipe[] recipes) {
-                Log.e("data done", String.valueOf(recipes.length));
-            }
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-            @Override
-            protected void onFailure(String message) {
-                Log.e("erroor", message);
-            }
-        };
-        networkHandler.execute();
+//        Media.Builder mediaBuilder = new Media.Builder()
+//                .mediaView(activityMainBinding.simpleExoPlayer)
+//                .videoLink("https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd9cb_4-press-crumbs-in-pie-plate-creampie/4-press-crumbs-in-pie-plate-creampie.mp4");
+//        final Media media = new Media(mediaBuilder, this);
+//        activityMainBinding.play.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                media.play();
+//            }
+//        });
+
     }
 
     @Override
@@ -41,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0 &&
                         grantResults[0] == PackageManager.PERMISSION_GRANTED &&
                         grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                    networkHandler.execute();
+
                 }else{
                     // TODO: error msg
                     Log.e("error","erro " + String.valueOf(grantResults[0]) + " " + String.valueOf(grantResults[1]));
---
+++ app/src/main/java/com/adja/apps/mohamednagy/bakingapp/media/Media.java
@@ -0,0 +1,96 @@
+package com.adja.apps.mohamednagy.bakingapp.media;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+/**
+ * Created by Mohamed Nagy on 3/23/2018.
+ */
+
+public class Media {
+    private SimpleExoPlayer mSimpleExoPlayer;
+    private Builder mBuilder;
+    private Context mContext;
+
+    public Media(Builder builder, Context context){
+        mBuilder = builder;
+        mContext = context;
+        init();
+    }
+
+    private void init(){
+        // initialize exoPlayer
+        prepareExoPlayer();
+        // Prepare Media
+        prepareMediaPlayer();
+    }
+
+
+    public void play(){
+        mSimpleExoPlayer.setPlayWhenReady(true);
+    }
+
+    public void release(){
+        mSimpleExoPlayer.stop();
+        mSimpleExoPlayer.release();
+        mSimpleExoPlayer = null;
+    }
+
+    private void prepareExoPlayer(){
+        TrackSelector trackSelector = new DefaultTrackSelector();
+        LoadControl loadControl = new DefaultLoadControl();
+        mSimpleExoPlayer = ExoPlayerFactory.newSimpleInstance(mContext, trackSelector, loadControl);
+        mBuilder.mSimpleExoPlayerView.setPlayer(mSimpleExoPlayer);
+    }
+
+    private void prepareMediaPlayer(){
+        String userAgent = Util.getUserAgent(mContext, "Baking");
+        MediaSource mediaSource = new ExtractorMediaSource(
+                mBuilder.mVideoUri,
+                new DefaultDataSourceFactory(mContext, userAgent),
+                new DefaultExtractorsFactory(),
+                null,
+                null
+        );
+        mSimpleExoPlayer.prepare(mediaSource);
+    }
+
+    public static class Builder{
+        private SimpleExoPlayerView mSimpleExoPlayerView;
+        private Uri mVideoUri;
+
+        public Builder mediaView(@NonNull SimpleExoPlayerView simpleExoPlayerView){
+            mSimpleExoPlayerView = simpleExoPlayerView;
+            return this;
+        }
+
+        public Builder defaultImage(@NonNull Bitmap bitmapImage){
+            assert mSimpleExoPlayerView != null;
+            mSimpleExoPlayerView.setDefaultArtwork(bitmapImage);
+            return this;
+        }
+
+        public Builder videoLink(@NonNull String videoLink){
+            mVideoUri = Uri.parse(videoLink);
+            return this;
+        }
+
+    }
+
+}
