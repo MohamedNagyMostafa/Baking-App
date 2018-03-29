19a31097352a1dd824ed47ffd7f3a7dcd42bf7c1
Mohamed Nagy <mohamednagy2015@outlook.com>
2018-03-29 04:42:03 -0700

    update style

--- app/src/main/java/com/adja/apps/mohamednagy/bakingapp/ui/screen/StepFragment.java
+++ app/src/main/java/com/adja/apps/mohamednagy/bakingapp/ui/screen/StepFragment.java
@@ -13,25 +13,22 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.adja.apps.mohamednagy.bakingapp.R;
-import com.adja.apps.mohamednagy.bakingapp.databinding.StepFragmentBinding;
-import com.adja.apps.mohamednagy.bakingapp.databinding.StepViewBinding;
+
 import com.adja.apps.mohamednagy.bakingapp.media.Media;
 import com.adja.apps.mohamednagy.bakingapp.media.sys.AudioFocusSystem;
 import com.adja.apps.mohamednagy.bakingapp.model.Step;
-import com.adja.apps.mohamednagy.bakingapp.ui.sys.VerticalStepSystem;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import ernestoyaquello.com.verticalstepperform.VerticalStepperFormLayout;
 
 
 /**
  * Created by Mohamed Nagy on 3/27/2018.
  */
 
-public class StepFragment extends Fragment implements VerticalStepSystem.ViewCreation {
+public class StepFragment extends Fragment  {
 
     @Nullable
     @Override
@@ -52,65 +49,9 @@ public class StepFragment extends Fragment implements VerticalStepSystem.ViewCre
                         "4. Sift together the flour, cocoa, and salt in a small bowl and whisk until mixture is uniform and no clumps remain. ",
                         "https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffdc9e_4-sift-flower-add-coco-powder-salt-brownies/4-sift-flower-add-coco-powder-salt-brownies.mp4",
                         ""));
-        // Set stepper sys.
-        VerticalStepSystem verticalStepSystem = new VerticalStepSystem(
-                steps,
-                this,
-                (VerticalStepperFormLayout) rootView.findViewById(R.id.verticalStepper),
-                getActivity()
-        );
 
 
         return rootView;
     }
 
-    @Override
-    public View createView(Step step, final VerticalStepperFormLayout verticalStepperFormLayout) {
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.step_view, null, false);
-        TextView descriptionText          = view.findViewById(R.id.descriptionText);
-        SimpleExoPlayerView exoPlayerView = view.findViewById(R.id.exoPlayerView);
-        Button previousStepButton         = view.findViewById(R.id.previousButton);
-        Button nextStepButton             = view.findViewById(R.id.nextButton);
-
-        descriptionText.setText(step.getDescription());
-        final Media media = setupMedia(exoPlayerView, step.getVideoLink(), step.getThumbnailURL());
-
-        nextStepButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                media.release();
-                verticalStepperFormLayout.goToNextStep();
-            }
-        });
-        previousStepButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                media.release();
-                verticalStepperFormLayout.goToPreviousStep();
-            }
-        });
-
-        media.play();
-        Log.e("called","done");
-        return view;
-    }
-
-    @Override
-    public void buildStepFormView(VerticalStepperFormLayout.Builder builder) {
-        builder.primaryColor(R.color.colorPrimary)
-                .primaryDarkColor(R.color.colorPrimary)
-                .init();
-    }
-
-    Media setupMedia(SimpleExoPlayerView simpleExoPlayerView, String videoLink, String thumbnailLink){
-        AudioFocusSystem audioFocusSystem = new AudioFocusSystem(getContext());
-        String mediaVideoLink = (videoLink.isEmpty())? thumbnailLink: videoLink;
-
-        return new Media.Builder(getContext())
-                .audioFocusSystem(audioFocusSystem)
-                .mediaView(simpleExoPlayerView)
-                .mediaStateListener(null)
-                .videoLink(mediaVideoLink)
-                .build();
-    }
 }
